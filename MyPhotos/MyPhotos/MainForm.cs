d5437fb6c79eed0dc6624068e96f2952c41c386a
leanhthinh <31847708+leanhthinh@users.noreply.github.com>
2017-10-09 18:41:40 +0700

    Photo Tool
    30 phút còn tiếp

---
+++ MyPhotos/MyPhotos/MainForm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyPhotos
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            SetTitleBar();
+        }
+
+        private void SetTitleBar()
+        {
+            Version ver = new Version(Application.ProductVersion);
+            Text = String.Format("MyPhotos {0:0}.{1:0}",
+                                 ver.Major, ver.Minor);
+        }
+
+    }
+}
